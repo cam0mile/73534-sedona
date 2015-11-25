520cc0938e508f1b76426c71c95c586c047df4b6
cam0mile <nik-nikolya@yandex.ru>
2015-11-25 21:12:50 +0300

    version 2

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,35 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var plumber = require("gulp-plumber");
+var notify = require("gulp-notify");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var browserSync = require('browser-sync').create();
 
 gulp.task("style", function() {
   return gulp.src("sass/style.{sass,scss}")
-    .pipe(plumber())
+    .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
     .pipe(sass())
     .pipe(postcss([
       autoprefixer({browsers: "last 2 versions"})
     ]))
-    .pipe(gulp.dest("css"));
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream())
+    .pipe(notify("hi"));
 });
 
-gulp.task("start", ["style"], function() {
+gulp.task("start", ["server","style"], function() {
   gulp.watch("sass/**/*.{sass,scss}", ["style"]);
+  gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
-
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 // Оставьте эту строку в самом конце файла
 require("./.gosha");
