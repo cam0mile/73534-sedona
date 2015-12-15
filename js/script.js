fc88380144872e36cfc7353918efedfb854d7de9
Ника Скобелева <nik-nikolya@yandex.ru>
2015-12-16 02:24:05 +0300

    js - подключила ajax + оживила гамбургер

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+
+var link = document.querySelector(".menu-section-open__link");
+var popup = document.querySelector(".menu-section")
+var close = document.querySelector(".menu-section__close");
+
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.add("menu-section-show");
+});
+
+close.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.remove("menu-section-show");
+});
+
+
+
+
+
